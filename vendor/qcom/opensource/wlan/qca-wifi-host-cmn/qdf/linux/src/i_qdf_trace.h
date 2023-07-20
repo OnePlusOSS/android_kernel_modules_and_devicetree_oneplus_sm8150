993439581252cf872cd3c184ed3eb9e0f286f4c3
pswbuild <pswbuild@oppo.com>
2023-07-20 11:50:03 +0800

    Synchronize code for oneplus GM1911_11_H.33

--- vendor/qcom/opensource/wlan/qca-wifi-host-cmn/qdf/linux/src/i_qdf_trace.h
+++ vendor/qcom/opensource/wlan/qca-wifi-host-cmn/qdf/linux/src/i_qdf_trace.h
@@ -32,6 +32,7 @@
 #ifdef CONFIG_QCA_MINIDUMP
 #include <linux/minidump_tlv.h>
 #endif
+#include <soc/oplus/system/oplus_project.h>
 
 #if !defined(__printf)
 #define __printf(a, b)
@@ -376,7 +377,16 @@ void __qdf_bug(void);
 #else /* CONFIG_SLUB_DEBUG */
 static inline void __qdf_bug(void)
 {
+#ifndef OPLUS_BUG_STABILITY
+//modify for: close bug in user build
 	BUG();
+#else /* OPLUS_BUG_STABILITY */
+	if (AGING == get_eng_version()) {
+		BUG();
+	} else {
+		WARN_ON(1);
+	}
+#endif /* OPLUS_BUG_STABILITY */
 }
 #endif /* CONFIG_SLUB_DEBUG */
 
